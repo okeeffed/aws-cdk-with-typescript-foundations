b9d5cb1826143ab00a5ee1203abb26a722e39a07
Dennis O'Keeffe <hello@dennisokeeffe.com>
2021-08-06 09:16:55 +1000

    Initial commit

---
+++ test/aws-cdk-with-typescript-foundations.test.ts
@@ -0,0 +1,13 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as AwsCdkWithTypescriptFoundations from '../lib/aws-cdk-with-typescript-foundations-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new AwsCdkWithTypescriptFoundations.AwsCdkWithTypescriptFoundationsStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT))
+});
